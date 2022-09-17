4c4d1bf08bf1e10a477e83bb3e405369e2175bf0
ThuyQuyenDo <106002830+ThuyQuyenDo@users.noreply.github.com>
2022-09-17 07:05:31 +0700

    UPdate

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*
+* WEB322 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Thuy Quyen Do 
+* Student ID: 154483218 
+* Date: Sep 16,2022
+*
+* Online (Cyclic) URL: https://chocolate-duckling-wrap.cyclic.app/
+*
+**/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
